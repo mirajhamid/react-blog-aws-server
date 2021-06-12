281a8d4512c2a5b724fe60e98333c3727a266dc9
Miraj Hamid <miraj.hamid@axiatadigitallabs.com>
2021-06-12 20:40:52 +0530

    mongo update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 //this is to read body data when using post
 import bodyParser from "body-parser";
+//using mongo client
+import { MongoClient } from "mongodb";
 
 const app = express();
 //here we gonna use the parser to read the request body with the any path
 app.use(bodyParser.json());
+
+const dbpw = "test123";
 //mock articleInfo data
 const articlesInfo = {
   "learn-react": {
@@ -24,26 +28,141 @@ const articlesInfo = {
 app.get("/hello", (req, res) => res.send("hello"));
 app.get("/hello/:name", (req, res) => res.send(`hello ${req.params.name}`));
 app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
-app.post("/api/articles/:name/upvote", (req, res) => {
-  const articleName = req.params.name;
-  articlesInfo[articleName].upvotes += 1;
-  res
-    .status(200)
-    .send(
-      `Aricle ${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+//
+// app.post("/api/articles/:name/upvote", (req, res) => {
+//   const articleName = req.params.name;
+//   articlesInfo[articleName].upvotes += 1;
+//   res
+//     .status(200)
+//     .send(
+//       `Aricle ${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+//     );
+// });
+
+app.post("/api/articles/:name/upvote", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const uri = `mongodb+srv://blog-user-mj:${dbpw}@miraj-cluster.mblki.mongodb.net/react-blog-aws?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const db = client.db("react-blog-aws");
+    //find info
+    const articleInfo = await db
+      .collection("articlesInfo")
+      .findOne({ name: articleName });
+
+    //update info
+    await db.collection("articlesInfo").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvote: articleInfo.upvote + 1,
+        },
+      }
     );
+
+    const updatedArticleInfo = await db
+      .collection("articlesInfo")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
 });
 
-app.post("/api/articles/:name/add-comment", (req, res) => {
-  const articleName = req.params.name;
+// app.post("/api/articles/:name/add-comment", (req, res) => {
 
-  //reading json body parameters
-  const { userName, commentText } = req.body;
+//   const articleName = req.params.name;
 
-  //add a new object for comments array based on the req
-  articlesInfo[articleName].comments.push({ userName, commentText });
+//   //reading json body parameters
+//   const { userName, commentText } = req.body;
 
-  res.status(200).send(articlesInfo[articleName]);
+//   //add a new object for comments array based on the req
+//   articlesInfo[articleName].comments.push({ userName, commentText });
+
+//   res.status(200).send(articlesInfo[articleName]);
+// });
+
+app.post("/api/articles/:name/add-comment", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    //reading json body parameters
+    const { userName, commentText } = req.body;
+    const uri = `mongodb+srv://blog-user-mj:${dbpw}@miraj-cluster.mblki.mongodb.net/react-blog-aws?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const db = client.db("react-blog-aws");
+    //find info
+    const articleInfo = await db
+      .collection("articlesInfo")
+      .findOne({ name: articleName });
+
+    //update info
+    await db.collection("articlesInfo").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ userName, commentText }),
+        },
+      }
+    );
+
+    const updatedArticleInfo = await db
+      .collection("articlesInfo")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
+
+// app.post("/api/articles-mongodb-onfly/:name", async (req, res) => {
+//   const articleName = req.params.name;
+//   const uri =
+//     "mongodb+srv://blog-user-mj:<pw>@miraj-cluster.mblki.mongodb.net/react-blog-aws?retryWrites=true&w=majority";
+//   const client = MongoClient.connect(
+//     uri,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     },
+//     async function (err, client) {
+//       if (err) {
+//         console.error("db error" + err);
+//         throw err;
+//       } else {
+//         console.log("db connected");
+//         const db = client.db("react-blog-aws");
+//         const articlesInfo = await db
+//           .collection("articlesInfo")
+//           .findOne({ name: articleName });
+//         console.log(articlesInfo);
+//         res.status(200).send(articlesInfo);
+//       }
+//     }
+//   );
+// });
+
+app.post("/api/articles-mongodb-onfly/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const uri = `mongodb+srv://blog-user-mj:${dbpw}@miraj-cluster.mblki.mongodb.net/react-blog-aws?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const db = client.db("react-blog-aws");
+    const articleInfo = await db
+      .collection("articlesInfo")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
 });
 
 app.listen(8000, () => console.log("listening to port 8000"));
